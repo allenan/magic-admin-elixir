3d4ec663a8503ca9e1ab336d1d8de0fc77be3720
Andrew Allen <allenan@helium.com>
2021-10-07 11:36:34 -0400

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,10 +4,11 @@ defmodule Magic.MixProject do
   def project do
     [
       app: :magic,
-      name: :magic_admin,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
       source_url: "https://github.com/allenan/magic-admin-elixir",
       homepage_url: "https://github.com/allenan/magic-admin-elixir"
@@ -27,6 +28,21 @@ defmodule Magic.MixProject do
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:jason, "~> 1.2"},
       {:eth, "~> 0.6.4"}
+    ]
+  end
+
+  defp description() do
+    "Magic admin Elixir SDK makes it easy to leverage Decentralized ID tokens to protect routes and restricted resources for your application."
+  end
+
+  defp package() do
+    [
+      name: "magic_admin",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/allenan/magic-admin-elixir"}
     ]
   end
 end
