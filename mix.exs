a43d595d79be8a9678d218d9fc6cc3c3ab6ba7df
Andrew Allen <allenan@helium.com>
2021-10-07 17:35:13 -0400

    bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Magic.MixProject do
   def project do
     [
       app: :magic,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
