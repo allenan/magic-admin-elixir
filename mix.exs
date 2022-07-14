7934b6ba7846800933cfa06a31345685448bb74f
Andrew Allen <allenan@helium.com>
2022-07-14 10:22:42 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Magic.MixProject do
   def project do
     [
       app: :magic_admin,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
