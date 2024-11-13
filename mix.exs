7f413f14291f0f93496c1ff984aaa9629a3ec077
Andrew Allen <allenan@nova-labs.com>
2024-11-13 13:35:52 -0500

    update to 0.10.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Magic.MixProject do
   def project do
     [
       app: :magic_admin,
-      version: "0.9.0",
+      version: "0.10.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
